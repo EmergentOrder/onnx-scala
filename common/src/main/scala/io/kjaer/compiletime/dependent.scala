18c0699c83893183031f1c422f1c420137fc70cb
Alex Merritt <lecaran@gmail.com>
2020-11-26 14:48:58 -0500

    Bump dotty/Scala3 version to 3.0.0-M2

--- common/src/main/scala/io/kjaer/compiletime/dependent.scala
+++ common/src/main/scala/io/kjaer/compiletime/dependent.scala
@@ -1,14 +1,13 @@
 //Author: Maxime Kjaer, taken from tf-dotty
 package io.kjaer.compiletime
 
-import scala.annotation.infix
 import scala.compiletime.ops.int._
 
 // Extensions on ints that allow scala.compiletime.ops to be dependently typed
 extension [X <: Int, Y <: Int](x: Int) {
-    @infix def add(y: Y): X + Y  = (x + y).asInstanceOf[X + Y]
-    @infix def sub(y: Y): X - Y  = (x - y).asInstanceOf[X - Y]
-    @infix def mul(y: Y): X * Y  = (x * y).asInstanceOf[X * Y]
-    @infix def lt(y: Y):  X < Y  = (x < y).asInstanceOf[X < Y]
-    @infix def le(y: Y):  X <= Y = (x <= y).asInstanceOf[X <= Y]
+    infix def add(y: Y): X + Y  = (x + y).asInstanceOf[X + Y]
+    infix def sub(y: Y): X - Y  = (x - y).asInstanceOf[X - Y]
+    infix def mul(y: Y): X * Y  = (x * y).asInstanceOf[X * Y]
+    infix def lt(y: Y):  X < Y  = (x < y).asInstanceOf[X < Y]
+    infix def le(y: Y):  X <= Y = (x <= y).asInstanceOf[X <= Y]
 }
